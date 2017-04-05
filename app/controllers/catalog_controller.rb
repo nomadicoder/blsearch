4299c2425e4b9becd92d4df9b4b242bc30ef0e2c
Steven Ng <steven.ng@temple.edu>
2017-04-05 16:13:54 -0400

    Make document URL clickable

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -117,7 +117,7 @@ class CatalogController < ApplicationController
     config.add_show_field 'release_date_display', label: 'Release Date'
 
     config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'url_fulltext_display', label: 'URL'
+    config.add_show_field 'url_fulltext_display', label: 'URL', helper_method: 'make_link'
     config.add_show_field 'url_suppl_display', label: 'More Information'
     config.add_show_field 'language_facet', label: 'Language'
     config.add_show_field 'published_display', label: 'Published'
